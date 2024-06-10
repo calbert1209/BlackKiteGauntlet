4401b3513b689f74206bde3894786418b291b5c6
Christian Albert <calbert1209@gmail.com>
2024-06-10 16:59:34 +0900

    initial commit, buildable sample app

---
+++ app/src/main/java/com/calbert/blackkitegauntlet/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.calbert.blackkitegauntlet.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun BlackKiteGauntletTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
