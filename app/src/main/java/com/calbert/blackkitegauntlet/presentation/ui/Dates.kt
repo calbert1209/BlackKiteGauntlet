0745b87996f20d4141a754bdd8bbc680f6d647e5
Christian Albert <calbert1209@gmail.com>
2024-06-23 23:37:05 +0900

    remove manual date control buttons

--- app/src/main/java/com/calbert/blackkitegauntlet/presentation/ui/Dates.kt
+++ app/src/main/java/com/calbert/blackkitegauntlet/presentation/ui/Dates.kt
@@ -1,19 +1,30 @@
 package com.calbert.blackkitegauntlet.presentation.ui
 
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.ButtonDefaults
+import androidx.wear.compose.material.Chip
+import androidx.wear.compose.material.CompactChip
 import androidx.wear.compose.material.Icon
 import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.OutlinedButton
 import androidx.wear.compose.material.Text
 import com.calbert.blackkitegauntlet.R
 import com.calbert.blackkitegauntlet.presentation.data.TidalEvent
@@ -82,15 +93,44 @@ fun DateAdjust(
     }
 }
 @Composable
-fun DateText(epochDays: Long) {
+fun DateText(epochDays: Long, onClick: () -> Unit) {
     val formatter = DateTimeFormatter.ofPattern("MM / dd")
+    Button(
+        modifier = Modifier.width(80.dp).padding(top = 16.dp),
+        shape = RoundedCornerShape(percent = 50),
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = MaterialTheme.colors.background,
+            contentColor = Color.White
+        )
+    ) {
+        Text(text = LocalDate.ofEpochDay(epochDays).format(formatter), fontSize = 18.sp)
+    }
 
-    Text(text = LocalDate.ofEpochDay(epochDays).format(formatter), fontSize = 18.sp, modifier = Modifier)
+}
+
+@Composable
+fun TidalExtremes(events: List<TidalEvent>?) {
+    if (events != null) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            events.forEach {event ->
+                TidalExtreme(event = event)
+            }
+        }
+    }
 }
 
 @Composable
 fun TidalExtreme(event: TidalEvent) {
     val symbol = if (event.type == "high") {"▲"} else {"▼"}
 
-    Text(text = "${symbol}: ${event.level}")
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start,
+
+    ) {
+        Text(modifier = Modifier.width(24.dp), text = symbol)
+        Text(modifier = Modifier.width(40.dp), text = "${event.level}")
+        Text(modifier = Modifier.width(54.dp), text = event.timestamp.substring(11, 16))
+    }
 }
--- app/src/main/java/com/calbert/blackkitegauntlet/presentation/ui/MainView.kt
+++ app/src/main/java/com/calbert/blackkitegauntlet/presentation/ui/MainView.kt
@@ -3,12 +3,16 @@ package com.calbert.blackkitegauntlet.presentation.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
@@ -19,6 +23,7 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.wear.compose.foundation.lazy.ScalingLazyColumn
 import androidx.wear.compose.foundation.lazy.rememberScalingLazyListState
+import androidx.wear.compose.material.Card
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Scaffold
 import androidx.wear.compose.material.TimeText
@@ -50,20 +55,22 @@ fun MainView(
         ScalingLazyColumn(
             modifier = Modifier
                 .fillMaxSize()
-                .background(MaterialTheme.colors.background)
-                .absoluteOffset(x = 0.dp, y = (-24).dp),
+                .background(MaterialTheme.colors.background),
+//                .absoluteOffset(x = 0.dp, y = (-24).dp),
             state = scrollState,
             verticalArrangement = Arrangement.Top
         ) {
+//            item {
+//                DateAdjust(
+//                    onChange = { viewModel.updateDate(it) },
+//                    onReset = { viewModel.resetDate() }
+//                )
+//            }
+            item { DateText(state.value.currentDate, onClick = {viewModel.resetDate()}) }
             item {
-                DateAdjust(
-                    onChange = { viewModel.updateDate(it) },
-                    onReset = { viewModel.resetDate() }
-                )
-            }
-            item { DateText(state.value.currentDate) }
-            state.value.extremes?.forEach {
-                item { TidalExtreme(event = it) }
+                Column (horizontalAlignment = Alignment.CenterHorizontally) {
+                    TidalExtremes(events = state.value.extremes)
+                }
             }
         }
 
