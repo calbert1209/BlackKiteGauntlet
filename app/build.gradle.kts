74516db5d187a1380876760afb0f943207cb3fa1
Christian Albert <calbert1209@gmail.com>
2024-06-28 23:16:20 +0900

    add lifecycle-runtime-compose library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation(libs.lifecycle.viewmodel.ktx)
     implementation(libs.lifecycle.viewmodel.compose)
     implementation(libs.room.runtime)
+    implementation(libs.lifecycle.runtime.compose)
     ksp(libs.room.compiler)
     implementation(libs.room.ktx)
     androidTestImplementation(platform(libs.compose.bom))
